74fa1af6fa09cb59fe80d101252646d9d8dd84d5
henning <henningsway@posteo.org>
2017-06-09 18:50:04 +0200

    mostly renaming in the parse2csv-file

--- R/parse2csv.R
+++ R/parse2csv.R
@@ -1,65 +1,69 @@
 #' Parse an Rmd-`r2anki`-**Template** to csv (for Import into Anki).
 #'
-#' This is an example for a link to an other package [roxygen2::roxygenize()] (from `roxygen2`).
 #' The goal of `parse2csv` is to compile an Anki-Rmd-Template.
-#'
 #'
 #' @param sourcefile path to `r2Anki`-Template
 #'
 #' @examples
-#' parse2csv()
+#' ## Define a Markdown-Template (read in from pkg-examples)
 #' @author Henning Bumann, \email{h_b@@posteo.de}
 #' @references https://en.wikipedia.org/wiki/Spaced_repetition
-#' @seealso [brocolors]
+#' @seealso [addin_r2anki_flashcard]
 #' @export
 
-
 parse2csv <- function(srcfile){
-  require(tibble)
+  require(tibble)  # where do I best put this requirement?
+                   # (and do i really need it?)
 
-  # render the source to html
+  # render the source to html and read in result
   rmarkdown::render(srcfile)
-  srcfile_html <- sub(pattern = "(.*?)\\..*$", replacement = "\\1.html", srcfile)
-  src <- readLines(srcfile_html)
-  unlink(srcfile_html)
+  html_srcfile <- sub(pattern = "(.*?)\\..*$",
+                      replacement = "\\1.html", srcfile)
+      # i think, the above statement could also be done with basefile() now.
+  lines_srcfile <- readLines(html_srcfile)
+  file.remove(html_srcfile)
 
   # parse html
   ## get relevant line numbers
-  card <- grep("<h4>", src)
-  back1 <- grep("<!-- start backside -->", src)
-  back2 <- grep("<!-- end backside -->", src)
+  card <- grep("<h4>", lines_srcfile)
+  start_backside <- grep("<!-- start backside -->", lines_srcfile)
+  end_backside <- grep("<!-- end backside -->", lines_srcfile)
 
   ## collapse multiline input
-  front <- unlist(lapply(Map(seq, card + 1, back1 - 1),
-                         function(x) paste(src[x], collapse = "<br>")))
-  back <- unlist(lapply(Map(seq, back1 + 1, back2),
-                        function(x) paste(src[x], collapse = "<br>")))
+  frontside <- unlist(lapply(Map(seq, card + 1, start_backside - 1),
+                         function(x) paste(lines_srcfile[x], collapse = "<br>")))
+  backside <- unlist(lapply(Map(seq, start_backside + 1, end_backside),
+                        function(x) paste(lines_srcfile[x], collapse = "<br>")))
 
-  ## combine in dataframe
-  df <- data.frame(front,
-                   back,
-                   id = src[card])
+  ## Combine in dataframe
+  cards <- data.frame(frontside,
+                   backside,
+                   id = lines_srcfile[card])
 
   ## Write to csv
-  write.table(df,sub(pattern = "(.*?)\\..*$", replacement = "\\1.tsv", srcfile),
+  write.table(cards, sub(pattern = "(.*?)\\..*$", replacement = "\\1.tsv", srcfile),
               sep = "\t",
               row.names = FALSE, col.names = FALSE,
               fileEncoding = "utf-8", quote = FALSE)
 }
 
-#parse2csv("examples/example_tasks.Rmd")
+# parse2csv("examples/example_tasks.Rmd")
 
-#length(back1) == length(back2)  # check, should be automated
-# back1 < card[-1]  # some checks, if some cards have just one side
+# Should the image-moving be done per default?
+  # It would be nice and convenient (should be default), but the anki-media-
+  # collection-folder would have to be specified. The csv should also be
+  # generated, if no or a wrong path to the collection-folder has been
+  # specified.
 
+# length(start_backside) == length(end_backside)  # check, should be automated
+#  start_backside < card[-1]  # some checks, if some cards have just one side
+  #! This actually seems important right now. :)
 
 # add print message, where the *.tsv-file can be found?
 # kann ich hier einen progress-bar einbauen?
-# welcher output wäre optimal?
-  # und wie gehe ich mit dem rmarkdown-ouptut am besten um?
 # add a variable for the file-path (default, same name, same directory)
 # add parsing for tags also
 # add some warnings to this file also.
-# add logic if either front or back are missing (requires some thinking.)
+# add logic if either front or backside are missing (requires some thinking.)
   # (values need to be between the two values of the other ... else its missing or so)
 # Later: parse multiple files (concatanate these files)
