9cb76af78357c36b159a1497a857fe9981145346
henning <henningsway@posteo.org>
2017-06-09 17:55:25 +0200

    misc

--- R/addin_r2anki_flashcard.R
+++ R/addin_r2anki_flashcard.R
@@ -3,7 +3,7 @@
 #' Call this function as an addin to insert a new flashcard at the cursor position.
 #'
 #' @export
-insert_r2anki_flashcard <- function() {
+addin_r2anki_flashcard <- function() {
   rstudioapi::insertText("# Topic
 <!-- Tags: *general tags for all cards in the topic* -->
   ## Subtopic
