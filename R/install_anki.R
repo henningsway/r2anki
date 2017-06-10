b1dde4128a3554928295e171edb8872d22daf47c
henning <henningsway@posteo.org>
2017-06-10 20:51:44 +0200

    added anki "install"-function

---
+++ R/install_anki.R
@@ -0,0 +1,8 @@
+#' Go to the Web and find yourself the up-to-date Anki-version for your OS.
+#'
+#' This function will be substituted by a more automatic approach.
+#' @export
+install_anki <- function() {
+  cat("For now, please install Anki manually. Thank you! :)")
+  browseURL("https://apps.ankiweb.net/#download")
+}
