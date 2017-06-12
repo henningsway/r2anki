953c94022c260e9693098c1e7d7b4ff8d8a9fb23
henning <henningsway@posteo.org>
2017-06-12 23:39:12 +0200

    simplified r2anki-function-call

--- R/r2anki.R
+++ R/r2anki.R
@@ -3,16 +3,18 @@
 #' @param path_collections.media_folder Path to the Anki-template. Should be similar to `"/home/usrname/.local/share/Anki2/User 1/collection.media/"`
 #' @param invoke_anki Should Anki be started and the tsv imported? Defaults to `FALSE`
 #' @export
-r2anki <- function(rmd_sourcefile, path_collections.media_folder, invoke_anki = FALSE) {
+r2anki <- function(rmd_sourcefile, invoke_anki = FALSE, ...) {
   # create tsv
   rmd2tsv(rmd_sourcefile)
 
   # copy images to anki
-  images2anki(rmd_sourcefile, path_collections.media_folder)
+  tsv_file <- paste0(tools::file_path_sans_ext(rmd_sourcefile), ".tsv")
+  images2anki(tsv_file, ...)
+
+  message("tsv-file successfully created.")  # change message, also check if file exists
 
   ## invoke anki with tsv-created
   if (invoke_anki){
-    tsv_file <- sub(pattern = "(.*?)\\..*$", replacement = "\\1.tsv", rmd_sourcefile)
     tsv2anki(tsv_file)
   }
 }
