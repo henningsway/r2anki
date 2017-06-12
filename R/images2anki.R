081d3b7d30a3c0ab83479a98d369f9a01310ead7
henning <henningsway@posteo.org>
2017-06-12 21:32:26 +0200

    Added helperfunctions to anki-dir as well as for the download. :)

--- R/images2anki.R
+++ R/images2anki.R
@@ -1,5 +1,7 @@
 #' Copy images from the `sourcefile` to the Anki `collection.media` folder
 #'
+#' You will have to call this function, when you have images in your anki cards. When you see empty plots in your Anki-Cards you should copy the images over to the anki-collections.media-folder.
+#'
 #' @param rmd_sourcefile Path to the rmd-template
 #' @param path_collections.media_folder Path to the Anki-template. Should be similar to /home/usrname/.local/share/Anki2/User 1/collection.media/"
 #' @param delete_directory Should the image files be deleted after the copying?
@@ -31,3 +33,28 @@ images2anki <- function(rmd_sourcefile,
 # src_test <- "inst/example-templates/testfile.Rmd"
 # c.m_test <- "inst/example-templates/test_collection.media/"
 # copy_images2anki(src_test, c.m_test)
+
+
+
+## define variables to anki-folder
+# inspired by blogdown:::bin_paths
+# should allow for custom path somehow
+anki_dir <- function (user = "User 1"){
+  if (is_windows()) {
+    path <-  Sys.getenv("APPDATA", "")
+    path <- if (dir.exists(path))
+      file.path(path, "Anki2")
+  }
+  else if (is_osx()) {
+    path <- "~/Library/Application Support/Anki2"
+    path <- if (dir.exists(path))
+      file.path(path)
+  }
+  else {
+    path <- "~/.local/share/Anki2"
+    path <- if (dir.exists(path))
+      file.path(path)
+  }
+  path <- file.path(path, user, "collection.media")
+  path
+}
--- R/install_anki.R
+++ R/install_anki.R
@@ -5,4 +5,22 @@
 install_anki <- function() {
   cat("For now, please install Anki manually. Thank you! :)")
   browseURL("https://apps.ankiweb.net/#download")
+  if(is_windows())
+    if (!require("installr")) install.packages("installr")
+    installr::install.URL("https://apps.ankiweb.net/downloads/current/anki-2.0.44.exe")  # use regexp to be immune to upcount in version-number
+  else if (is_osx()) {
+    message("osx download not implemented yet.")
+  }
+  else if (is_linux()) {
+    message("linux download not implemented yet.")
+  }
 }
+
+
+## Find out operating-system, copyied from "blogdown"-pkg
+is_linux <- function ()
+  Sys.info()[["sysname"]] == "Linux"
+is_osx <- function ()
+  Sys.info()[["sysname"]] == "Darwin"
+is_windows <- function ()
+  .Platform$OS.type == "windows"
