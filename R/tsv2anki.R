429dd2351bd738db389a4831e24ee3456aa1e366
henning <henningsway@posteo.org>
2017-06-12 23:41:32 +0200

    system-call kinda works (not really)

--- R/tsv2anki.R
+++ R/tsv2anki.R
@@ -1,6 +1,7 @@
 #' Start Anki (on Linux) and import a *.tsv-file to your Collections
 #'
 #' @export
-tsv2anki <- function(tsv) {
-  system(paste("anki", tsv))
+tsv2anki <- function(tsv_file) {
+  if (is_linux())
+    system2("anki", tsv_file)
 }
